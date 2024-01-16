72c1f525574ade100c00385d095bfb22c4a638e0
karanghai9 <kg9811422163@gmail.com>
2024-01-16 05:12:38 +0100

    Changes related to Ant.

---
+++ Demo/src/sourceCode/Addition.java
@@ -0,0 +1,20 @@
+package sourceCode;
+public class Addition 
+{
+	public static int add(int a,int b)
+	{
+		return a+b;  
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		int a = 14;
+		int b = 5;
+		int answer;
+		answer = add(a,b);
+		System.out.println(answer);
+	}
+
+}
+ 
